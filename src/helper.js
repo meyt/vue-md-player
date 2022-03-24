a539e38421c51d53d2ed86b2a7281b6f6f11a9c2
meyt <pasd3000@gmail.com>
2022-03-24 23:10:30 +0430

    Drop lodash

--- src/helper.js
+++ src/helper.js
@@ -1,13 +1,15 @@
-import padStart from 'lodash/padStart'
+export function padStart (a, b, c) {
+  return (new Array(b || 2).join(c || 0) + (a || c || 0)).slice(-b)
+}
 
 export function secondsToTime (secs) {
-  var hours = Math.floor(secs / (60 * 60))
+  let hours = Math.floor(secs / (60 * 60))
 
-  var divisorForMinutes = secs % (60 * 60)
-  var minutes = Math.floor(divisorForMinutes / 60)
+  const divisorForMinutes = secs % (60 * 60)
+  let minutes = Math.floor(divisorForMinutes / 60)
 
-  var divisorForSeconds = divisorForMinutes % 60
-  var seconds = Math.ceil(divisorForSeconds)
+  const divisorForSeconds = divisorForMinutes % 60
+  let seconds = Math.ceil(divisorForSeconds)
   hours = padStart(hours, 2, '0')
   minutes = padStart(minutes, 2, '0')
   seconds = padStart(seconds, 2, '0')
